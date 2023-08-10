6bdf5fc9c7f386c6b46eba7bb52e80d5e484e578
Summer <na.gao@lincolnuni.ac.nz>
2023-08-11 07:10:21 +1200

    add login and logout route
    i tested admin account login, it was successful.

--- app.py
+++ app.py
@@ -43,6 +43,67 @@ def sample():
 
     return render_template('sample.html', sample_list=sample_list)
 
+# http://localhost:5000/login/ - this will be the login page, we need to use both GET and POST requests   
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+   
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        user_password = request.form['password']
+
+        # Check if account exists
+        cursor = get_cursor()
+        cursor.execute("SELECT * FROM user_account WHERE username = %s", (username,))
+        account = cursor.fetchone()
+
+        if account is not None:
+            password_hash = account[3]
+
+            # Check if the hashed password matches the one stored in the database
+            if bcrypt.checkpw(user_password.encode('utf-8'), password_hash.encode('utf-8')):
+                # If the passwords match, log the user in
+                session['loggedin'] = True
+                session['id'] = account[0]
+                session['username'] = account[1]
+                session['is_member'] = account[4]
+                session['is_instructor'] = account[5]
+                session['is_admin'] = account[6]
+                session['is_root'] = account[7]
+
+                if session['is_member'] == 1:
+                # Redirect to home page
+                    return redirect(url_for('home'))
+                elif session['is_instructor'] == 1:
+                    return redirect(url_for('staff_dashboard'))
+                elif session['is_admin'] == 1:
+                    return redirect(url_for('admin_dashboard'))
+                else:
+                    return redirect(url_for('sample'))
+            else:
+                # Add a message to prompt the use
+                return 'Incorrect password!'
+        else:
+            # Account doesn't exist or username incorrect
+            return 'Incorrect username!'
+
+    # Show the login form with message (if any)
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+
+    # Add a flash message to prompt the user
+    return "'You have been logged out.', 'success'"
+
+   
+
 
 if __name__ == '__main__':
     app.run(debug=True)
