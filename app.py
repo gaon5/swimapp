3315b95f01d26d33ea9f6c7639b63691bc6e7966
NickyTing <37256814+NickyTing@users.noreply.github.com>
2023-08-10 12:52:02 +1200

    Sample
    Sample

--- app.py
+++ app.py
@@ -1,13 +1,51 @@
 from flask import Flask, url_for, request, redirect, render_template, session
+from datetime import datetime
+import mysql.connector
+import config
+import math
 import bcrypt
 
+
+
+# When you gonna start, pip install -r requirements.txt
+
+
 app = Flask(__name__)
+app.config.from_object(config)
+<<<<<<< HEAD
+=======
+app.secret_key = 'aHn6Zb7MstRxC8vEoF2zG3B9wQjKl5YD'
+
+>>>>>>> cda50d995c0d1b868c87aa4eba6ab2b8de3f6229
+
+db_conn = None
+connection = None
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def get_cursor():
+    global db_conn
+    global connection
+    connection = mysql.connector.connect(user=config.dbuser,
+                                         password=config.dbpass,
+                                         host=config.dbhost,
+                                         database=config.dbname,
+                                         autocommit=True)
+    db_conn = connection.cursor()
+    return db_conn
+
+
+@app.route('/sample', methods=['GET', 'POST'])
+def sample():
+    sample_value = 1
+    sql_data = get_cursor()
+    sql = """SELECT * FROM sample_database WHERE sample_id=%s;"""
+    sql_value = (sample_value,)
+    sql_data.execute(sql, sql_value)
+    sample_list = sql_data.fetchall()
+    sql_data.close()
+
+    return render_template('sample.html', sample_list=sample_list)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
