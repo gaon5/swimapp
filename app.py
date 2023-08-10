12e2369e6ea04e817be1b10be6b96ed42f0203cf
Summer <na.gao@lincolnuni.ac.nz>
2023-08-11 11:54:14 +1200

    register route added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import mysql.connector
 import config
 import math
 import bcrypt
+import re
 
 
 
@@ -42,6 +43,50 @@ def sample():
     sql_data.close()
 
     return render_template('sample.html', sample_list=sample_list)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    msg = ''
+
+    if request.method == 'POST':
+        # Ensure necessary fields are present
+        fields = ['username', 'password', 'email', 'first_name', 'last_name', 'phone_number']
+        if not all(field in request.form for field in fields):
+            return render_template('register.html', msg="Please fill out the form!")
+
+        username, password, email, first_name, last_name, phone_number= (request.form[field] for field in fields)
+
+        # Check criteria
+        with get_cursor() as cursor:
+            cursor.execute('SELECT email, username FROM user_account WHERE email = %s OR username = %s', (email, username))
+            existing_data = cursor.fetchone()
+           
+            if existing_data:
+                existing_email, existing_username = existing_data
+                if existing_email == email:
+                    msg = 'An account with this email already exists!'
+                if existing_username == username:
+                    msg += ' An account with this username already exists!'
+            elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+                msg = 'Invalid email address!'
+            elif not re.match(r'^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$', password):
+                msg = 'Password must contain at least 8 characters, one letter and one number!'
+            elif not re.match(r'[A-Za-z0-9]+', username):
+                msg = 'Username must contain only characters and numbers!'
+            else:
+                salt = bcrypt.gensalt()
+                hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+                cursor.execute('INSERT INTO user_account (username, password, email, is_member) VALUES (%s, %s, %s, 1)', (username, hashed, email))
+                cursor.execute('SELECT user_id from user_account WHERE username = %s', (username,))
+                user_id = cursor.fetchone()[0]
+
+                cursor.execute('INSERT INTO member (user_id, first_name, last_name, phone_number) VALUES (%s, %s, %s, %s)', (user_id, first_name, last_name, phone_number))
+                connection.commit()
+
+                msg = 'You have successfully registered!'
+
+    return render_template('register.html', msg=msg)
 
 
 if __name__ == '__main__':
