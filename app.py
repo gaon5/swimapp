f254e19e2253f30b0f7d5b4f3aa846698e442795
LeoZhao <Leo.Zhao@lincolnuni.ac.nz>
2023-08-10 14:44:00 +1200

    8_10
    8_10

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ import config
 import math
 import bcrypt
 
-
-
 # When you gonna start, pip install -r requirements.txt
 
 
 app = Flask(__name__)
 app.config.from_object(config)
 app.secret_key = 'aHn6Zb7MstRxC8vEoF2zG3B9wQjKl5YD'
-
 
 db_conn = None
 connection = None
@@ -42,6 +39,36 @@ def sample():
     sql_data.close()
 
     return render_template('sample.html', sample_list=sample_list)
+
+
+@app.route('/selfInformation', methods=['GET', 'POST'])
+def self_information():
+    # if 'loggedIn' in session:
+    if 1:
+        # user_id = session["user_id"]
+        user_id = 3
+        sql_data = get_cursor()
+        if request.method == 'GET':
+            sql = """SELECT * FROM `admin` AS a
+                        LEFT JOIN `user_account` AS u ON a.user_id=u.user_id
+                        WHERE a.user_id=%s;"""
+            sql_value = (user_id,)
+            sql_data.execute(sql, sql_value)
+            admin_list = sql_data.fetchall()
+            sql_data.close()
+            return render_template('adminInformation.html', admin_list=admin_list)
+        else:
+            title = request.form.get('title')
+            first_name = request.form.get('first_name')
+            last_name = request.form.get('last_name')
+            email = request.form.get('email')
+            phone_number = request.form.get('phone_number')
+            user_id = request.form.get('user_id')
+            # 检查email
+
+            return redirect(url_for('self_information'))
+    else:
+        pass
 
 
 if __name__ == '__main__':
