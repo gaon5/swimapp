b038a4a35fa6f551876e9a3b1c2de57125c13d17
Duomude1412_Silver Wang <125863557+Duomude1412@users.noreply.github.com>
2023-08-14 10:57:38 +1200

    update

--- app.py
+++ app.py
@@ -50,61 +50,56 @@ def home():
 @app.route('/profile', methods=['GET'])
 def profiles():
 
-    if "loggedin" in session:
+    # if "loggedin" in session:
+    if 1:
+        # user_id = session['id']
+        user_id = 4
 
         sql_data = get_cursor()
-        user_id = session['id']
         get_role_sql = """SELECT * from user_account WHERE user_id = %s;"""
-        sql_data.execute(get_role_sql,user_id)
+        sql_data.execute(get_role_sql,(user_id,))
         result = sql_data.fetchone()
         if result[4]:
             role = "member"
+            profile_detail_sql = """SELECT member.member_id,member.user_id,member.first_name,member.last_name,member.phone_number,
+                                    member.detailed_information,member.street_name,member.birth_date,member.health_information,
+                                    title.title,city.city,region.region 
+                                    FROM member 
+                                    LEFT JOIN title ON member.title_id = title.title_id
+                                    LEFT JOIN city ON member.city_id = city.city_id
+                                    LEFT JOIN region ON member.region_id = region.region_id
+                                    WHERE user_id = %s;"""
+            sql_data.execute(profile_detail_sql,(user_id,))
+            profile_details = sql_data.fetchone()
+            title = profile_details[9]
+
+
         elif result[5]:
             role = "instructor"
+            profile_detail_sql = """SELECT instructor.instructor_id,instructor.user_id,instructor.first_name,instructor.last_name,
+                                    instructor.phone_number, instructor.detailed_information,
+                                    title.title
+                                    FROM instructor 
+                                    LEFT JOIN title ON instructor.title_id = title.title_id 
+                                    WHERE user_id = %s;"""
+            sql_data.execute(profile_detail_sql,(user_id,))
+            profile_details = sql_data.fetchone()
+            title = profile_details[-1]
+
         elif result[6]:
             role = "admin"
+            profile_detail_sql = """SELECT admin.admin_id,admin.user_id,admin.first_name,admin.last_name,
+                                    admin.phone_number, title.title
+                                    FROM admin
+                                    LEFT JOIN title ON admin.title_id = title.title_id 
+                                    WHERE user_id = %s;"""
+            sql_data.execute(profile_detail_sql,(user_id,))
+            profile_details = sql_data.fetchone()
+            title = profile_details[-1]
         elif result[7]:
             role = "root"
     
-        if role =="member":
-            profile_detail_sql = """SELECT * from member WHERE user_id = %s;"""
-        elif role == "instructor":
-            profile_detail_sql = """SELECT * from instructor WHERE user_id = %s;"""
-        elif role == "admin":
-            profile_detail_sql = """SELECT * from admin WHERE user_id = %s;"""
-    
-        sql_data.execute(profile_detail_sql,user_id)
-        profile_details = sql_data.fetchone()
-
-        if role =="member":
-            member_id = profile_details[0]
-            title_id = profile_details[2]
-            first_name = profile_details[3]
-            last_name = profile_details[4]
-            phone_number = profile_details[5]
-        elif role == "instructor":
-            instructor_id = profile_details[0]
-            title_id = profile_details[2]
-            first_name = profile_details[3]
-            last_name = profile_details[4]
-            phone_number = profile_details[5]
-            detailed_information  = profile_details[6]
-        elif role == "admin":
-            admin_id = profile_details[0]
-            title_id = profile_details[2]
-            first_name = profile_details[3]
-            last_name = profile_details[4]
-            phone_number = profile_details[5]
-
-        title_sql = """SELECT * from title WHERE title_id = %s;"""
-        sql_data.execute(title_sql,title_id)
-        title = sql_data.fetchone()[0]
-
-
-        return render_template("profile.html", 
-                               member_id = member_id, title = title, first_name = first_name, last_name = last_name,
-                               phone_number = phone_number,instructor_id = instructor_id, detailed_information = detailed_information, 
-                               admin_id = admin_id,role = role)
+        return render_template("profile.html", role = role, profile_details = profile_details,title = title,)
 
     else:
         return "Please Log In"
@@ -112,65 +107,31 @@ def profiles():
 @app.route('/member_list', methods=['GET'])
 def member_list():
     sql_data = get_cursor()
-    user_id = session['id']
-    get_role_sql = """SELECT * from user_account WHERE user_id = %s;"""
-    sql_data.execute(get_role_sql,user_id)
+
+    # user_id = session['id']
+    user_id = 4
+
+    get_role_sql = """SELECT * FROM user_account WHERE user_id = %s;"""
+    sql_data.execute(get_role_sql,(user_id,))
     result = sql_data.fetchone()
-    if result[4]:
-        role = "member"
-    elif result[5]:
-        role = "instructor"
-    elif result[6]:
+    if result[6]:
         role = "admin"
-    elif result[7]:
-        role = "root"
+        member_list_sql = """SELECT member.member_id,member.user_id,member.first_name,member.last_name,member.phone_number,
+                            member.detailed_information,member.street_name,member.birth_date,member.health_information,
+                            title.title,city.city,region.region 
+                            FROM member 
+                            LEFT JOIN title ON member.title_id = title.title_id
+                            LEFT JOIN city ON member.city_id = city.city_id
+                            LEFT JOIN region ON member.region_id = region.region_id
+                            ORDER BY member_id;"""
+        sql_data.execute(member_list_sql,)
+        member_list_result = sql_data.fetchall()
+        print(member_list_result)
+        return render_template("member_list.html",role = role,member_list_result = member_list_result,)
 
-    member_list_sql = """SELECT * from member ;"""
-    sql_data.execute(member_list_sql,)
-    member_list_result = sql_data.fetchall()
-
-    title_sql = """SELECT * from title WHERE title_id = %s;"""
-    sql_data.execute(title_sql,member_list_result['title_id'])
-    title = sql_data.fetchone()[0]
-
-    if role == "admin":
-        return render_template("member_list.html",role = role,member_list_result = member_list_result,title = title)
     else:
-        return "You could not read this page"
+        return "You could not approach member list"
 
-@app.route('/member_details', methods=['GET'])
-def member_details():
-    UserID = request.args.get('UserID')
-    sql_data = get_cursor()
-    user_id = session['id']
-    get_role_sql = """SELECT * from user_account WHERE user_id = %s;"""
-    sql_data.execute(get_role_sql,user_id)
-    result = sql_data.fetchone()
-    if result[4]:
-        role = "member"
-    elif result[5]:
-        role = "instructor"
-    elif result[6]:
-        role = "admin"
-    elif result[7]:
-        role = "root"
-
-    if role == "admin":
-        profile_detail_sql = """SELECT * from member WHERE user_id = %s;"""
-        sql_data.execute(profile_detail_sql,UserID)
-        profile_details = sql_data.fetchone()
-        member_id = profile_details[0]
-        title_id = profile_details[2]
-        first_name = profile_details[3]
-        last_name = profile_details[4]
-        phone_number = profile_details[5]
-        title_sql = """SELECT * from title WHERE title_id = %s;"""
-        sql_data.execute(title_sql,title_id)
-        title = sql_data.fetchone()[0]
-
-        return render_template("profile.html", 
-                               member_id = member_id, title = title, first_name = first_name, last_name = last_name,
-                               phone_number = phone_number, role = "member")
 
 
 if __name__ == '__main__':
