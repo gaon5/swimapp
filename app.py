d6c711bc8135f38920b3d5e8785b3953356a384f
LeoZhao <Leo.Zhao@lincolnuni.ac.nz>
2023-08-14 16:20:50 +1200

    Edit
    Edit

--- app.py
+++ app.py
@@ -40,80 +40,27 @@ def get_cursor():
 #     return render_template('sample.html', sample_list=sample_list)
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template("base.html")
-
-
-@app.route('/profile', methods=['GET'])
-def profiles():
-    # if "loggedin" in session:
-    if 1:
-        # user_id = session['id']
-        user_id = 4
-        sql_data = get_cursor()
-        get_role_sql = """SELECT * from user_account WHERE user_id = %s;"""
-        sql_data.execute(get_role_sql, (user_id,))
-        result = sql_data.fetchone()
-        if result[4]:
-            role = "member"
-            profile_detail_sql = """SELECT member.member_id,member.user_id,member.first_name,member.last_name,member.phone_number,
-                                    member.detailed_information,member.street_name,member.birth_date,member.health_information,
-                                    title.title,city.city,region.region 
-                                    FROM member 
-                                    LEFT JOIN title ON member.title_id = title.title_id
-                                    LEFT JOIN city ON member.city_id = city.city_id
-                                    LEFT JOIN region ON member.region_id = region.region_id
-                                    WHERE user_id = %s;"""
-            sql_data.execute(profile_detail_sql, (user_id,))
-            profile_details = sql_data.fetchone()
-            title = profile_details[9]
-        elif result[5]:
-            role = "instructor"
-            profile_detail_sql = """SELECT instructor.instructor_id,instructor.user_id,instructor.first_name,instructor.last_name,
-                                    instructor.phone_number, instructor.detailed_information,
-                                    title.title
-                                    FROM instructor 
-                                    LEFT JOIN title ON instructor.title_id = title.title_id 
-                                    WHERE user_id = %s;"""
-            sql_data.execute(profile_detail_sql, (user_id,))
-            profile_details = sql_data.fetchone()
-            title = profile_details[-1]
-        elif result[6]:
-            role = "admin"
-            profile_detail_sql = """SELECT admin.admin_id,admin.user_id,admin.first_name,admin.last_name,
-                                    admin.phone_number, title.title
-                                    FROM admin
-                                    LEFT JOIN title ON admin.title_id = title.title_id 
-                                    WHERE user_id = %s;"""
-            sql_data.execute(profile_detail_sql, (user_id,))
-            profile_details = sql_data.fetchone()
-            title = profile_details[-1]
-        elif result[7]:
-            role = "root"
-        return render_template("profile.html", role=role, profile_details=profile_details, title=title, )
-    else:
-        return "Please Log In"
-
-
-@app.route('/member_list', methods=['GET'])
-def member_list():
+@app.route('/user_list', methods=['GET'])
+def user_list():
     # if "loggedin" in session:
     if 1:
         # if session['admin'] == 1 or session['root'] == 1:
         if 1:
             sql_data = get_cursor()
-            sql = """SELECT member.member_id,member.user_id,member.first_name,member.last_name,member.phone_number,
-                    member.detailed_information,member.street_name,member.birth_date,member.health_information,
-                    title.title,city.city,region.region 
-                    FROM member 
-                    LEFT JOIN title ON member.title_id = title.title_id
-                    LEFT JOIN city ON member.city_id = city.city_id
-                    LEFT JOIN region ON member.region_id = region.region_id
-                    ORDER BY member_id;"""
+            sql = """SELECT m.user_id,m.first_name,m.last_name,m.phone_number,t.title,u.username,u.email,m.state FROM member AS m
+                        LEFT JOIN `user_account` AS u ON m.user_id=u.user_id
+                        LEFT JOIN `title` AS t ON t.title_id=m.title_id
+                        WHERE m.state = 1;"""
             sql_data.execute(sql)
-            member_list_result = sql_data.fetchall()
-            return render_template("member_list.html", member_list_result=member_list_result)
+            member_list = sql_data.fetchall()
+            sql = """SELECT i.user_id,i.first_name,i.last_name,i.phone_number,t.title,u.username,u.email,i.state FROM instructor AS i
+                        LEFT JOIN `user_account` AS u ON u.user_id=i.user_id
+                        LEFT JOIN `title` AS t ON t.title_id=i.title_id
+                        WHERE i.state = 1;"""
+            sql_data.execute(sql)
+            instructor_list = sql_data.fetchall()
+            sql_data.close()
+            return render_template("user_list.html", member_list=member_list, instructor_list=instructor_list)
         else:
             return url_for("/")
     else:
